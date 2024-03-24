793c004b9259a10f3be7c2cc6e918edc45a07b4e
smohan67 <siddhu.mohan6@gmail.com>
2024-03-24 18:38:53 -0500

    comments

--- Gambling Simulator/Assets/Scripts/NPC/NPCDialouge.cs
+++ Gambling Simulator/Assets/Scripts/NPC/NPCDialouge.cs
@@ -17,22 +17,23 @@ namespace NPC
             isInteracting = true;
             Debug.Log(Text);
             isInteracting = false;
+            //show display for dialogue
         }
         
 
         public void OnInteractSelected()
         {
-            
+            //highlight?
         }
 
         public void InteractSelectedLoop()
         {
-            
+            //unhighlight?
         }
 
         public void OnInteractionDeselected()
         {
-            
+            //hig ui display for dialogue
         }
     }
 }
