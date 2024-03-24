4ab81f8802f4c1546c827788848a4a80be7b1679
sagore1263 <146508032+sagore1263@users.noreply.github.com>
2024-03-24 16:56:38 -0500

    car script and gameObject

---
+++ Gambling Simulator/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    public float speed = 5f;
+    public float delay = 2f;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+    
+    void Start()
+    {
+        MoveToRandomPoint();
+    }
+
+    
+    void Update()
+    {
+        if (!isMoving) {
+            Invoke("MOveToRandomPoint", delay);
+        }
+        else {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            if (transform.position == targetPosition) {
+                isMoving = false;
+            }
+        }
+    }
+    void MoveToRandomPoint()
+    {
+        float randomFactor = Random.Range(0f, 1f);
+        targetPosition = Vector3.Lerp(startPoint.position, endPoint.position, randomFactor);
+        isMoving = true;
+    }
+}
