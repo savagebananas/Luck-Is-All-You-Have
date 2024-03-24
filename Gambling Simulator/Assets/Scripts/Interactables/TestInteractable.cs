48528b77ac260d9ccf2ff31d47d6da1820f3651b
smohan67 <siddhu.mohan6@gmail.com>
2024-03-24 02:05:16 -0500

    changes

---
+++ Gambling Simulator/Assets/Scripts/Interactables/TestInteractable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Interactables
+{
+    public class TestInteractable : MonoBehaviour, IInteractable
+    {
+        public void OnInteract()
+        {
+            Debug.Log("Interaction called.");
+        }
+
+        public void OnInteractSelected()
+        {
+            Debug.Log("Current interactable set to this.");
+        }
+
+        public void InteractSelectedLoop()
+        {
+            Debug.Log("Current interactable continuously being set to this.");
+        }
+
+        public void OnInteractionDeselected()
+        {
+            Debug.Log("Current interactable deselected.");
+        }
+    }
+}
