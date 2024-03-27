7fdba66d719c2917c7381b98683268380095b8c2
savagebananas <bruno7405@gmail.com>
2024-03-27 17:04:26 -0500

    Randomization of car spawner

--- Gambling Simulator/Assets/Scripts/Car/CarSpawnerScript.cs
+++ Gambling Simulator/Assets/Scripts/Car/CarSpawnerScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class CarSpawnerScript : MonoBehaviour
 {
-    public GameObject carPrefab;
+    public List<GameObject> carPrefabs;
     public float spawnRate;
     public int sortingLayerOrder;
     private float timer = 0;
@@ -25,7 +25,7 @@ public class CarSpawnerScript : MonoBehaviour
 
         else {
             // Instantiate car
-            var car = Instantiate(carPrefab, transform.position, transform.rotation);
+            var car = Instantiate(carPrefabs[(int) Random.RandomRange(0, carPrefabs.Count)], transform.position, transform.rotation);
             
             // Set car move script to move left or right
             car.GetComponent<CarMoveScript>().goingLeft = leftMovingCars;
