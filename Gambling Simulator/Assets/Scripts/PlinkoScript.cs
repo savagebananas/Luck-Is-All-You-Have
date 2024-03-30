dfd3319d03608241b30b5df28328f13b78569051
sagore1263 <146508032+sagore1263@users.noreply.github.com>
2024-03-30 00:54:16 -0500

    Updated drag race and plinko

--- Gambling Simulator/Assets/Scripts/PlinkoScript.cs
+++ Gambling Simulator/Assets/Scripts/PlinkoScript.cs
@@ -7,33 +7,56 @@ public class PlinkoScript : MonoBehaviour
     public Rigidbody2D r1;
     public Rigidbody2D r2;
     public Rigidbody2D r3;
+    public Rigidbody2D r4;
+    public Rigidbody2D r5;
     private bool ballReleased = false;
     public int toggle ;
+    public int specialnum;
+    public int tries;
     // Start is called before the first frame update
     void Start()
     {
-        toggle = Random.Range(1, 4) ;
+        toggle = Random.Range(1, 5) ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (toggle == 1 && !ballReleased)
+        if (Input.GetKeyDown(KeyCode.Space) && tries!=0)
         {
-            changeGravity(r1, 1f);
-            ballReleased = true;
-        }
-        if (toggle == 2 && !ballReleased)
-        {
-            changeGravity(r2, 1f);
-            ballReleased = true;
-        }
-        if (toggle == 3 && !ballReleased)
-        {
-            changeGravity(r3, 1f);
-            ballReleased = true;
-        }
+            if (specialnum == 0 && !ballReleased)
+            {
+                specialnum = 100;
+                changeGravity(r4, 1);
+                ballReleased = true;
+            }
 
+            if (toggle == 1 && !ballReleased)
+            {
+                changeGravity(r1, 1f);
+                ballReleased = true;
+                specialnum--;
+            }
+            if (toggle == 2 && !ballReleased)
+            {
+                changeGravity(r2, 1f);
+                ballReleased = true;
+                specialnum--;
+            }
+            if (toggle == 3 && !ballReleased)
+            {
+                changeGravity(r3, 1f);
+                ballReleased = true;
+                specialnum--;
+            }
+            if (toggle == 4 && !ballReleased)
+            {
+                changeGravity(r5, 1f);
+                ballReleased = true;
+                specialnum--;
+            }
+            tries--;
+        }
 
     }
     void changeGravity(Rigidbody2D r,float newVal) {
