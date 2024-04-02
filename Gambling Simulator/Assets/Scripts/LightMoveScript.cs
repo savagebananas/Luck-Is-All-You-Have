20f66dc83c4481fcfc4e16bc18ac600e07cef474
sagore1263 <146508032+sagore1263@users.noreply.github.com>
2024-04-02 13:20:44 -0500

    Flickering lights in slotmachine

---
+++ Gambling Simulator/Assets/Scripts/LightMoveScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMoveScript : MonoBehaviour
+{
+    private bool movingRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 18)
+        {
+            movingRight = false;
+        }
+        else if (transform.position.x < -13)
+        {
+            movingRight = true;
+        }
+        if (movingRight)
+        {
+            transform.position += (25 * Time.deltaTime * Vector3.right);
+        }
+        else
+        {
+            transform.position -= (25 * Time.deltaTime * Vector3.right);
+        }
+    }
+}
