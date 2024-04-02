09a7c820e7b957595871b412f74bb245ac4ec881
sagore1263 <146508032+sagore1263@users.noreply.github.com>
2024-04-02 03:06:15 -0500

    Lamp time controller

--- Gambling Simulator/Assets/timeManager.cs
+++ Gambling Simulator/Assets/timeManager.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class timeManager : MonoBehaviour
 {
     public ParticleSystem stars;
-
+    public GameObject[] lamps;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -18,6 +19,11 @@ public class timeManager : MonoBehaviour
     {
         if ((0 <= TimeSystem.time && TimeSystem.time < 6) || (18 <= TimeSystem.time && TimeSystem.time < 23.5))
         {
+            foreach (GameObject lamp in lamps)
+            {
+                Light2D light = lamp.GetComponentInChildren<Light2D>();
+                light.intensity = 5;
+            }
             stars.emissionRate = 125;
         }
         else if (6 <= TimeSystem.time && TimeSystem.time < 6.5f)
@@ -26,6 +32,7 @@ public class timeManager : MonoBehaviour
         }
         else if (6.5f <= TimeSystem.time && TimeSystem.time < 7)
         {
+
             stars.emissionRate = 50;
         }
         else if (7 <= TimeSystem.time && TimeSystem.time < 7.5f)
@@ -34,11 +41,29 @@ public class timeManager : MonoBehaviour
         }
         else if (7.5f <= TimeSystem.time && TimeSystem.time < 17)
         {
+
             stars.emissionRate = 0;
         }
         else if (17f <= TimeSystem.time && TimeSystem.time < 18)
         {
             stars.emissionRate = 50;
+        }
+
+        if ((0 <= TimeSystem.time && TimeSystem.time < 6) || (18 <= TimeSystem.time && TimeSystem.time < 23.5))
+        {
+            foreach (GameObject lamp in lamps)
+            {
+                Light2D light = lamp.GetComponentInChildren<Light2D>();
+                light.intensity = 5;
+            }
+        }
+        else
+        {
+            foreach (GameObject lamp in lamps)
+            {
+                Light2D light = lamp.GetComponentInChildren<Light2D>();
+                light.intensity = 0;
+            }
         }
     }
 }
